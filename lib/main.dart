594b86e85472c5e17728bd39ef3fc1129fbb7fb5
SANJAY-P-M <pmsanjay2003@gmail.com>
2023-09-04 16:11:08 +0530

    Working on ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,36 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: InputPage(),
+      // Learning ThemeData class
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0xFF0a0e21),
+          primary: Color(0xFF0a0e21),
+          secondary: Color(0xFF1D1F33),
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.white,
+          size: 100,
+        ),
+        textTheme: TextTheme(
+          displayMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+          displaySmall: TextStyle(
+            color: Color(0xFF60626F),
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+          displayLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        scaffoldBackgroundColor: Color(0xFF0a0e21),
+      ),
     );
   }
 }
@@ -21,14 +51,83 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+        title: Center(
+          child: Text('BMI CALCULATOR'),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 26),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 24, horizontal: 12),
+                      color: Theme.of(context).colorScheme.secondary,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.male,
+                            color: IconTheme.of(context).color,
+                          ),
+                          Text(
+                            "MALE",
+                            style: Theme.of(context).textTheme.displayMedium,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 24, horizontal: 12),
+                      color: Theme.of(context).colorScheme.secondary,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.female,
+                            color: IconTheme.of(context).color,
+                          ),
+                          Text(
+                            "FEMALE",
+                            style: Theme.of(context).textTheme.displayMedium,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                color: Theme.of(context).colorScheme.secondary,
+                child: Column(
+                  children: [
+                    Text(
+                      "HEIGHT",
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                    Text(
+                      "183",
+                      style: Theme.of(context).textTheme.displayLarge,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
